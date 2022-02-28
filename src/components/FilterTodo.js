e0ffd96a4cbd35c79fedaeb5d1a55ee6cdb22060
Kécia Santos <keciakaroline@gmail.com>
2022-02-28 19:11:03 +0100

    Created Filter for todolist

--- src/components/FilterTodo.js
+++ src/components/FilterTodo.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function FilterTodo() {
   return (
     <form>
-      <div class="select">
-        <select name="todos" class="filter-todo">
+      <div className="select">
+        <select name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
