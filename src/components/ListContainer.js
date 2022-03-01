3bb55607c840f288612325389f6ef1f8d35c5ae6
Kécia Santos <keciakaroline@gmail.com>
2022-03-01 17:39:41 +0100

    Added loop to run all the list <li>

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -1,10 +1,22 @@
 import React from "react";
+import List from "./List";
+import "./styles/ListContainer.css";
 
-export default function ListContainer({ todos }) {
+export default function ListContainer({ todos, setTodos }) {
   console.log(todos);
   return (
     <div className="List">
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <List
+            todos={todos}
+            setTodos={setTodos}
+            key={todo.id}
+            text={todo.text}
+            todo={todo}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
