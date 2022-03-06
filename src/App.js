d404ee03300f5006214cc297d8f7fc5fbbdee988
Kécia Santos <keciakaroline@gmail.com>
2022-03-06 18:43:35 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Form from "./Form";
+import ListContainer from "./ListContainer";
+import FilterTodo from "./FilterTodo";
 import "./App.css";
-import TodoApp from "./components/TodoApp";
 
 function App() {
+  const [inputText, setinputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState("All");
   return (
     <div className="App">
-      <TodoApp />
+      <Header />
+      <header>
+        <h1>T O D O</h1>
+      </header>
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setinputText={setinputText}
+      />
+      <ListContainer todos={todos} setTodos={setTodos} />
+      <FilterTodo />
+      <Footer />
     </div>
   );
 }
