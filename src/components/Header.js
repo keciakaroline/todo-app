d329e6f73a8c4e6ab8bba66983197e4240bcbc15
Kécia Santos <keciakaroline@gmail.com>
2022-03-18 18:13:11 +0100

    Added rotation to the button

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,24 +3,27 @@ import "./styles/Header.css";
 import { ReactComponent as SunIcon } from "./styles/images/icon-sun.svg";
 import { ReactComponent as MoonIcon } from "./styles/images/icon-moon.svg";
 
-export default function Header() {
-  let [lightMode, setLightMode] = useState(false);
-  const handleClick = () => {
-    setLightMode(!lightMode);
+export default function Header({ theme, setTheme, themeToggler }) {
+  const [rotate, setRotate] = useState(false);
+  // change theme when clicking the icon
+  const handleTheme = () => {
+    themeToggler();
+    setRotate(true);
+    setTimeout(() => {
+      setRotate(false);
+    }, 1000);
   };
-
-  if (lightMode === true) {
-    console.log(true);
-    document.body.classList.add("light-mode");
-  } else {
-    document.body.classList.remove("light-mode");
-  }
 
   return (
     <div className="Header">
-      <span className="icon-change" onClick={handleClick}>
-        {lightMode ? <MoonIcon /> : <SunIcon />}
-      </span>
+      <h1 className="title">TODO</h1>
+      <button
+        aria-label="theme-change"
+        className={`theme-change ${rotate ? "rotate" : ""}`}
+        onClick={handleTheme}
+      >
+        {theme === "dark" ? <MoonIcon /> : <SunIcon />}
+      </button>
     </div>
   );
 }
