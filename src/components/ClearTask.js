8ff03eb349d3c0ded40be09e8267bc86549e7497
Kécia Santos <keciakaroline@gmail.com>
2022-03-21 23:09:26 +0100

    divided and changed position

--- src/components/ClearTask.js
+++ src/components/ClearTask.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./styles/ClearTask.css";
 
-export default function ClearTask({ todos, setTodos }) {
+// create itemsLeft
+export const TaskLeft = ({ todos }) => {
+  const itemsLeft = () => {
+    let completedItems = [];
+    let totalItems = todos.length;
+    completedItems = todos.filter((todo) => todo.completed === true);
+    return totalItems - completedItems.length;
+  };
+  return (
+    <div className="items-left">
+      <span>{itemsLeft()} items left</span>
+    </div>
+  );
+};
+
+export const ClearTask = ({ todos, setTodos }) => {
   // delete all tasks
   const clearTask = () => {
     let deleted = todos.filter((todo) => {
@@ -9,24 +24,11 @@ export default function ClearTask({ todos, setTodos }) {
     });
     setTodos(deleted);
   };
-
-  const itemsLeft = () => {
-    let completedItems = [];
-    let totalItems = todos.length;
-    completedItems = todos.filter((todo) => todo.completed === true);
-    return totalItems - completedItems.length;
-  };
-
   return (
     <div className="ClearTask">
-      <div className="itensLeft">
-        <span>{itemsLeft()} items left</span>
-      </div>
-      <div>
-        <button className="clearBtn" value="clear" onClick={clearTask}>
-          Clear Completed
-        </button>
-      </div>
+      <button className="clearBtn" value="clear" onClick={clearTask}>
+        Clear Completed
+      </button>
     </div>
   );
-}
+};
